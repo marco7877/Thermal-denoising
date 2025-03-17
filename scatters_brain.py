a6f49ac5e30a6b1ea80382d790bc391773d933e9
marco7877 <marcoantonio7877@gmail.com>
2025-03-17 15:31:00 +0100

    working function for scatter plot with plotnine

--- scatters_brain.py
+++ scatters_brain.py
@@ -8,15 +8,32 @@ Created on Fri Oct 20 15:23:25 2023
 
 from nilearn import image
 from nilearn import signal
-from nilearn.masking import apply_mask
-from nilearn.maskers import NiftiMasker
+from nilearn.masking import (
+        apply_mask,
+        compute_epi_mask
+        )
 from scipy import stats
 import numpy as np
 import pandas as pd
 import os
 import argparse
-from plotnine import  *
-
+from plotnine import (
+        ggplot,
+        aes,
+        geom_point,
+        scale_x_continuous,
+        scale_y_continuous,
+        xlab,
+        ylab,
+        geom_smooth,
+        geom_abline,
+        theme,
+        panel_background,
+        panel_grid,
+        panel_border,
+        element_blank,
+        element_rect)
+from itertools import combinations
 #####################################################################################
 ###### Arguments ####################################################################
 #####################################################################################
@@ -24,34 +41,63 @@ from plotnine import  *
 parser=argparse.ArgumentParser(description="""Generates scatterplots with plotnine
         from fMRI data with only one brik """)
 parser.add_argument("--source_directory", default=None, type=str,
-        help="Full path to the BIDS directory")
+        help="Full path to the source directory")
+parser.add_argument("--subjects", default=None, type=list,
+        help=""" subjects to iterate and do within method comparison
+        i.e. subjects=(sub-001, sub-002, sub-003)""")
+parser.add_argument("--tasks", default=None, type=list,
+        help=""" task to iterate and do within method comparison per task/run
+        i.e. tasks=(mppca,nordic,hydra,tmppca)""")
 parser.add_argument("--methods", default=None, type=list,
         help=""" method to iterate and compare i.e. methods=(mppca,nordic,hydra,tmppca)""")
-parser.add_argument("--nii_pattern", default=None, type=str,
-        help="""file pattern to search with subject, task and method replaced with
-        SUB, TSK, and MTHD. i.e. SUB_ses-1_TSK_OC_part-mag_bold_MTHD_gm_union""")
+parser.add_argument("--nii_pattern", default=None, type=list,
+        help="""list of ordered strings with the elements to iterate over. They 
+        must follow the pattern ([SUB]_ses-1,[_TSK]_OC_part-mag_bold,[_MTHD]_gm_union)
+        where whats within square braces are the ommited variables we will be iterating""")
+parser.add_argument("--overwrite", default=True, type=bool,
+        help=""" Haults program if scatter plots exist. Default behaviour is True""")
 #####################################################################################
 ###### Arguments ####################################################################
 #####################################################################################
 
 args = parser.parse_args()
-bids_dir = args.bids_dir
-mprageize_dir = args.mprageize_dir
-mprageize = mprageize_dir + "/3dMPRAGEize"
+source_directory = args.source_directory
+subjects = args.subjects
+tasks = args.tasks
+methods = args.methods
+nii_pattern= args.nii_pattern
+source_directory = args.source_directory
 overwrite = args.overwrite
+#####################################################################################
+###### Functions ####################################################################
+#####################################################################################
+def scatter_plot(subject,task,methodx,methody,directory=source_directory,automask=True,mask_file=""):
+    epi_file1=directory+"/"+subject+"_ses-1_"+task+"_OC_part-mag_bold_"+methodx+"_gm_union.nii.gz"
+    epi_file2=directory+"/"+subject+"_ses-1_"+task+"_OC_part-mag_bold_"+methody+"_gm_union.nii.gz"
+    if automask == True:
+        # We assume both epi are on the same space and mask
+        mask=compute_epi_mask(epi_file1,exclude_zeros=True)
+    else:
+        mask=compute_epi_mask(mask_file,exclude_zeros=True)
+    masked_datay=apply_mask(epi_file1,mask)
+    masked_datax=apply_mask(epi_file1,mask)
+    df_plot=pd.DataFrame({'X':masked_datax,'Y':masked_datay})
+    plotnine=(ggplot(df_plot,aes("X","Y"))
+            +geom_point(alpha=0.3,colour="gray")
+            +geom_smooth(method="lm")
+            +geom_abline(intercept=0,slope=1,colour="red",linetype="dotted")
+            +scale_x_continuous(limits=[0,150])
+            +scale_y_continuous(limits=[0,150])
+            +xlab(task+" "+methodx)
+            +ylab(task+" "+methody)
+            +theme(
+                panel_background=element_rect(fill="white"),
+                panel_grid=element_blank(),
+                panel_border=element_blank())
+            )
+    plotnine.draw()
+    plotnine.save(directory+"/"+subject+task+"vanilla-"+methody+".png",verbose=False)
+#####################################################################################
+###### Main      ####################################################################
+#####################################################################################
 
-#parser.add_argument("--tsnr_extention", default=None, type=str,
-#                    help="""Extention of the target tsnr file to be look upon
-#                    i.e., if my target file has the name:
-#                    sub-001_task-HABLA1200_masked_epi_gm_ocDenoised_tsnr.nii.gz
-#                    then, OC_extention=task-HABLA1200_masked_epi_gm_ocDenoised_tsnr.nii.gz""")
-#
-#source_directory = "/bcbl/home/public/MarcoMotion/Resting_State/analysis/"
-#task="HABLA1200"
-#tsnr_class="1200-OHBM"
-#tsnr_extention="task-"+task+"_OC_part-mag_bold_"
-
-#ext="_gm.nii.gz"
-#methods=["hydra", "nordic","vanilla", "tmmpca", "mppca", "nordic_magn"]
-#labels=["hydra", "nordic","vanilla", "tmmpca", "mppca", "nordic_magn"]
-#voxel="2.4*2.4*2.4mm"
